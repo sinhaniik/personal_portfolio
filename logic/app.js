dc12dff57eab433cc6912b3df361ef7ecd231572
Nikhil Sinha <nikhilsinha198@gmail.com>
2023-02-16 20:59:07 +0530

    add link to the buttons

---
+++ logic/app.js
@@ -0,0 +1,56 @@
+// THE GITHUB
+
+$('.github').click(function (e) {
+	const target1 = e.target.classList[0];
+	const target2 = e.target.classList[2];
+	console.log(target1);
+	console.log(target2);
+
+	if (target1 === 'fa-github' || 'github') {
+		window.location = 'https://github.com/sinhaniik';
+	} else if (target2 === 'fa-github' || 'github') {
+		window.location = 'https://github.com/sinhaniik';
+	}
+});
+
+// THE TWITTER
+$('.twitter').click(function (e) {
+	const target1 = e.target.classList[0];
+	const target2 = e.target.classList[2];
+	console.log(target1);
+	console.log(target2);
+
+	if (target1 === 'fa-twitter' || 'twitter') {
+		window.location = 'https://twitter.com/sinhaniik';
+	} else if (target2 === 'fa-twitter' || 'twitter') {
+		window.location = 'https://twitter.com/sinhaniik';
+	}
+});
+
+//THE LINKEDIN
+$('.linkedin').click(function (e) {
+	const target1 = e.target.classList[0];
+	const target2 = e.target.classList[2];
+	console.log(target1);
+	console.log(target2);
+
+	if (target1 === 'fa-linkedin' || 'linkedin') {
+		window.location = 'https://www.linkedin.com/in/sinhaniik/';
+	} else if (target2 === 'fa-linkedin' || 'linkedin') {
+		window.location = 'https://www.linkedin.com/in/sinhaniik/';
+	}
+});
+
+//THE HASHNODE
+$('.hashnode').click(function (e) {
+	const target1 = e.target.classList[0];
+	const target2 = e.target.classList[2];
+	console.log(target1);
+	console.log(target2);
+
+	if (target1 === 'fa-hashnode' || 'hashnode') {
+		window.location = 'https://sinhaniik.hashnode.dev/';
+	} else if (target2 === 'fa-hashnode' || 'hashnode') {
+		window.location = 'https://sinhaniik.hashnode.dev/';
+	}
+});
